5e76a81cc7fab05b122bff6d56ada7c5814da48b
Paulina Wozniak <S00176242@mail.itsligo.ie>
2019-11-16 15:41:25 +0000

    c

---
+++ Ad Aeternum/Assets/PaulinasWork/Scripts/InventoryItems/InventoryPanel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using System.Windows.Forms;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryPanel : MonoBehaviour
+{
+
+    public RectTransform lstGridInventory;
+    public GameObject GridButtonPrefab;
+    public ItemDetailsPanel detailsPanel;
+
+    private void Update()
+    {
+        
+    }
+
+    void Start()
+    {
+        LoadList(GameManager.MasterCollection);
+    }
+
+    public void LoadList(List<Item> items)
+    {
+
+        for(int i =0; i < items.Count;i++)
+        {
+            GameObject button = Instantiate(GridButtonPrefab, lstGridInventory);
+
+            GridButton gridButton = button.GetComponent<GridButton>();
+            gridButton.InitializeGridButton(items[i].ID, items[i].IconName);
+
+            gridButton.Selected += GridButton_OnSelected;
+        }
+    }
+
+    private void GridButton_OnSelected(int itemID)
+    {
+        detailsPanel.SetItem(itemID);
+    }
+}
---
+++ Ad Aeternum/Assets/PaulinasWork/Scripts/InventoryItems/UserProfile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class UserProfile
+{
+    public string Username;
+    public Color Color;
+    public string ImageName;
+
+    [NonSerialized]//no going to be saved to JSON
+    public Sprite Image;
+}
