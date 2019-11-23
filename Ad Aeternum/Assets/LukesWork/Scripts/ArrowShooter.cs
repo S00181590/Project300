91cb7d9e40f06278e5659eea48adeff2c1517f20
S00181282 <35927198+S00181282@users.noreply.github.com>
2019-11-23 21:01:29 +0000

    Arrow Pointing

--- Ad Aeternum/Assets/LukesWork/Scripts/ArrowShooter.cs
+++ Ad Aeternum/Assets/LukesWork/Scripts/ArrowShooter.cs
@@ -2,48 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ArrowShooter : Arrow
+public class ArrowShooter : MonoBehaviour
 {
     float speed = 8f;
     public GameObject arrow;
     //public GameObject arrowSpawn;
-    Vector3 spawnPoint;
+    public GameObject spawnPoint;
     public CameraMoveController cam;
     public PlayerMoveController player;
     int count = 0;
     GameObject shootingArrow;
     Rigidbody rb;
 
-    public override void MakeArrow()
+    GameObject spawnedArrow;
+
+    private void Update()
     {
-        spawnPoint = player.transform.localPosition + new Vector3(0.8f, 0, 0);
+        MakeArrow();
+    }
+
+    public void MakeArrow()
+    {
+        //spawnPoint = player.transform.localPosition + new Vector3(0.8f, 0, 0);
 
         if (cam.bowAim)
         {
             if (count <= 0)
             {
-                var spawnedArrow = Instantiate(arrow, spawnPoint, Quaternion.identity);
+                spawnedArrow = Instantiate(arrow, spawnPoint.transform.position, Quaternion.identity);
                 rb = spawnedArrow.GetComponent<Rigidbody>();
 
                 count++;
             }
 
-            arrow.transform.position = spawnPoint;
-            arrow.transform.rotation = Quaternion.Euler(90, 0, 0);
+            spawnedArrow.transform.localPosition = new Vector3(spawnPoint.transform.position.x, spawnPoint.transform.position.y, spawnPoint.transform.position.z);
+            spawnedArrow.transform.localRotation = Quaternion.Euler(cam.camTransform.rotation.eulerAngles.x + 90, cam.camTransform.rotation.eulerAngles.y, cam.camTransform.localRotation.z);
+            //spawnedArrow.transform.localRotation = Quaternion.Euler(0, 0, cam.transform.rotation.eulerAngles.z);
+            //spawnedArrow.transform.Rotate(player.transform.position, -cam.transform.rotation.eulerAngles.z);
 
 
-            if (Input.GetMouseButton(0))
+            if (Input.GetMouseButtonDown(0))
             {
+                cam.bowAim = false;
+                rb.velocity = Vector3.zero;
+                rb.AddForce(spawnedArrow.transform.forward * 0.2f, ForceMode.Impulse);
+                Debug.DrawLine(spawnedArrow.transform.position, spawnedArrow.transform.rotation.eulerAngles * 0.2f);
                 Invoke("ShootArrow", 1);
             }
-
-            base.MakeArrow();
         }
     }
 
     void ShootArrow()
     {
         var direction = cam.camTransform.rotation.eulerAngles;
-        rb.AddForce(direction * speed);
+        rb.AddForce(-direction * 0.2f); 
     }
 }
