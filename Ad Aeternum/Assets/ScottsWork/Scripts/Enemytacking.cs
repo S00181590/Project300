80c49301cb517c487ce7d2b1302eddd171877fef
Scott Gallagher <35927142+s00178222@users.noreply.github.com>
2019-11-25 11:54:24 +0000

    updates
    added more options enemys path finding and tracking

---
+++ Ad Aeternum/Assets/ScottsWork/Scripts/Enemytacking.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemytacking : NavMeshMover
+{
+    public GameObject PlayerChracther;
+    public float FollowDistance;
+    public Vector3 startPoistion;
+
+   public override void Start()
+    {
+        startPoistion = transform.position;
+        base.Start();
+    }
+
+    
+   private void Update()
+    {
+        if(Vector3.Distance(transform.position,PlayerChracther.transform.position)<=FollowDistance)
+        {
+            agent.stoppingDistance = 2;
+            MoveTo(PlayerChracther);
+        }
+
+        else
+        {
+            agent.stoppingDistance = 0;
+            MoveTo(startPoistion);
+        }
+    }
+}
--- Ad Aeternum/Assets/ScottsWork/Scripts/SettingMenu.cs
+++ Ad Aeternum/Assets/ScottsWork/Scripts/SettingMenu.cs
@@ -1,51 +1,92 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
+
 public class SettingMenu : MonoBehaviour
 {
-    Resolution[] resolutions;
+    public GameOptions gameOptions;
 
-    public Dropdown resoutiondropdpwnsetting;
+    public Toggle fullScreenToggle;
+    public Dropdown resoutiondropdownsetting;
+    public Dropdown TextureQualitySettings;
+    public Slider MusicVolumeSlider;
 
-     void Start()
+    public Resolution[] resolutions; //stores all resoultions and returns
+
+
+    public Button buttonapply;
+    public AudioSource Musicsources;
+
+    void OnEnable()
     {
-     resolutions =  Screen.resolutions;
-        resoutiondropdpwnsetting.ClearOptions();
+        gameOptions = new GameOptions();
+
+        fullScreenToggle.onValueChanged.AddListener(delegate { SetFullScreen(); });
+        resoutiondropdownsetting.onValueChanged.AddListener(delegate { setResoultion(); });
+        TextureQualitySettings.onValueChanged.AddListener(delegate { setTextureQuality(); });
+        MusicVolumeSlider.onValueChanged.AddListener(delegate { SettingtheVolume(); });
+        buttonapply.onClick.AddListener(delegate { OnButtonApplyClick(); });
+
+
+        resoutiondropdownsetting.ClearOptions();
         List<string> resoultionoptions = new List<string>();
 
-        int CurrentResoultion = 0;
-        for (int i = 0; i < resolutions.Length; i++)
+
+        resolutions =  Screen.resolutions;
+       
+        foreach (Resolution resolution in resolutions)
         {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
-            resoultionoptions.Add(option);
-            if(resolutions[i].height ==Screen.currentResolution.height &&
-                resolutions[i].height ==Screen.currentResolution.width)
-            {
-                CurrentResoultion = i;
-
-            }
+            resoutiondropdownsetting.options.Add(new Dropdown.OptionData(resolution.ToString()));
         }
-        resoutiondropdpwnsetting.AddOptions(resoultionoptions);
-        resoutiondropdpwnsetting.value = CurrentResoultion;
-        resoutiondropdpwnsetting.RefreshShownValue();
+
+        LoadSettings();
     }
 
-    public void setResoultion (int Resoultionsetting)
+    public void setResoultion()
     {
-        Resolution resoultion = resolutions[Resoultionsetting];
-        Screen.SetResolution(resoultion.width, resoultion.height, Screen.fullScreen);
-    }
-    public AudioMixer audioMixer;
-    public void SettingtheVolume(float Volume)
-    {
-        audioMixer.SetFloat("volume", Volume);
+        Screen.SetResolution(resolutions[resoutiondropdownsetting.value].width, resolutions[resoutiondropdownsetting.value].height, Screen.fullScreen);
+        gameOptions.ResolutionIndex = resoutiondropdownsetting.value;
     }
 
-    public void SetFullScreen(bool isFullScreen)
+    public void SettingtheVolume()
     {
-        Screen.fullScreen = isFullScreen;
+        Musicsources.volume = gameOptions.MusicVolume = MusicVolumeSlider.value;
     }
+
+    public void SetFullScreen()
+    {
+
+       gameOptions.FullScreen = Screen.fullScreen = fullScreenToggle.isOn;
+        
+    }
+
+    public void setTextureQuality()
+    {
+        QualitySettings.masterTextureLimit = TextureQualitySettings.value;
+    }
+
+    public void SaveSettings()
+    {
+        string jsonData = JsonUtility.ToJson(gameOptions,true);
+        File.WriteAllText(Application.persistentDataPath + "/gamesettings.json", jsonData);
+    }
+    public void OnButtonApplyClick()
+    {
+        SaveSettings();
+    }
+    public void LoadSettings()
+    {
+        gameOptions = JsonUtility.FromJson<GameOptions>(File.ReadAllText(Application.persistentDataPath + "/gamesettings.json"));
+        MusicVolumeSlider.value = gameOptions.MusicVolume;
+        resoutiondropdownsetting.value = gameOptions.ResolutionIndex;
+        TextureQualitySettings.value = gameOptions.TextureQualtity;
+        fullScreenToggle.isOn = gameOptions.FullScreen;
+        Screen.fullScreen = gameOptions.FullScreen;
+        resoutiondropdownsetting.RefreshShownValue();
+    }
+
 }
