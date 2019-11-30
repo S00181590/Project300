98e0966cc5ab86602e4173f1a9d0c5d5f8b4e435
Scott Gallagher <35927142+s00178222@users.noreply.github.com>
2019-11-30 20:35:21 +0000

    updated

--- Ad Aeternum/Assets/ScottsWork/Scripts/spider.cs
+++ Ad Aeternum/Assets/ScottsWork/Scripts/spider.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class spider : MonoBehaviour
+public class spider : SmallBallMover
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        base.Start();
     }
 
     // Update is called once per frame
