c7d8aa2e86a6564285342e258e7babd4111c73b3
Luke O'Reilly <S00181282@mail.itsligo.ie>
2019-12-09 04:34:25 +0000

    Testing camera collision

---
+++ Ad Aeternum/Assets/LukesWork/Scripts/CameraCollision.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollision : MonoBehaviour
+{
+    public float minDistance = 1f;
+    public float maxDistance = 4f;
+    public float smooth = 10f;
+    Vector3 dir;
+    public Vector3 dirAdjusted;
+    public GameObject player;
+    public float distance;
+    public LayerMask ignore;
+
+    void Start()
+    {
+        dir = transform.localPosition.normalized;
+        distance = transform.localPosition.magnitude;
+    }
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+
+        if (Physics.Linecast(transform.position - new Vector3(0, 0, 10), player.transform.position, out hit, ignore))
+        {
+            //distance = Mathf.Clamp(hit.distance, minDistance, maxDistance);
+            transform.localPosition = hit.point.normalized;
+        }
+        else
+        {
+            transform.localPosition = new Vector3(0, 0.5f, -4f);
+            //distance = maxDistance;
+        }
+
+        //transform.localPosition = Vector3.Lerp(transform.localPosition, dir * distance, Time.deltaTime * smooth);
+    }
+}
---
+++ Ad Aeternum/Assets/LukesWork/Scripts/CameraInvisibleObjects.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraInvisibleObjects : MonoBehaviour
+{
+    public LayerMask LayerMask;
+    RaycastHit[] hitResults;
+    List<string> objectsBlockingView = new List<string>();
+
+    void Update()
+    {
+        hitResults = Physics.RaycastAll(Camera.main.transform.position, Camera.main.transform.forward, 4, LayerMask);
+
+        if (hitResults.Length > 0)
+        {
+            //Hide all blocking objects
+
+            ShowAllObjects();
+
+            for (int i = 0; i < hitResults.Length; i++)
+            {
+                if (!objectsBlockingView.Contains(hitResults[i].collider.gameObject.name))
+                {
+                    objectsBlockingView.Add(hitResults[i].collider.gameObject.name);
+                    HideObject(hitResults[i].collider.gameObject.name);
+                }
+            }
+        }
+        else
+        {
+            //Show all previously hidden objects
+
+            ShowAllObjects();
+        }
+    }
+
+    private void ShowAllObjects()
+    {
+        for (int i = 0; i < objectsBlockingView.Count; i++)
+        {
+            ShowObject(objectsBlockingView[i]);
+        }
+
+        objectsBlockingView.Clear();
+    }
+
+    private void HideObject(string name)
+    {
+        GameObject foundObject = GameObject.Find(name);
+        MeshRenderer renderer = foundObject.GetComponent<MeshRenderer>();
+        Color originalColour = renderer.material.color;
+        originalColour.a = 0.1f;
+        renderer.material.color = originalColour;
+    }
+
+    private void ShowObject(string name)
+    {
+        GameObject foundObject = GameObject.Find(name);
+        MeshRenderer renderer = foundObject.GetComponent<MeshRenderer>();
+        Color originalColour = renderer.material.color;
+        originalColour.a = 1.0f;
+        renderer.material.color = originalColour;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        //Gizmos.DrawLine(Camera.main.transform.position, Camera.main.transform.position + (Camera.main.transform.forward * 4));
+    }
+}
