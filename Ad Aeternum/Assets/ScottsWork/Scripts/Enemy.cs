c3d33dcc525fa2ca9e6a463c5a134d4f29c58d55
Scott Gallagher <35927142+s00178222@users.noreply.github.com>
2020-01-06 14:57:13 +0000

    ai
    bragn working on enemies ai and player stats

--- Ad Aeternum/Assets/ScottsWork/Scripts/Enemy.cs
+++ Ad Aeternum/Assets/ScottsWork/Scripts/Enemy.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class flyingskull : NavMeshMover
+public class Enemy : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        base.Start();
+        
     }
 
     // Update is called once per frame
---
+++ Ad Aeternum/Assets/ScottsWork/Scripts/EnemytrackinAndFacesPlayer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemytrackinAndFacesPlayer : NavMeshMover
+{
+    protected GameObject MainPlayer;
+    public float FollowDistance;
+    public Vector3 startPoistion;
+
+    Transform target;
+
+
+    public override void Start()
+    {
+        startPoistion = transform.position;
+        MainPlayer = GameObject.FindGameObjectWithTag("Player");
+         
+        base.Start();
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, MainPlayer.transform.position) <= FollowDistance)
+        {
+            agent.stoppingDistance = 3f;
+            MoveTo(MainPlayer);
+        }
+        else
+        {
+            agent.stoppingDistance = 5;
+            MoveTo(startPoistion);
+        }
+
+        float distance = Vector3.Distance(target.position, transform.position);
+        if (distance <= agent.stoppingDistance)
+        {
+            FaceTarget();//calling face target to update what the enemy looking at
+        }
+
+    }
+
+    void FaceTarget() // makes the enemey or abjecht face the player when he attacks 
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion FollowDistance = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = FollowDistance;
+        //transform.rotation = Quaternion.Slerp(transform.rotation, FollowDistance, Time.deltaTime * 5f);
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;//color for the enemy follow distance raidus 
+        Gizmos.DrawWireSphere(transform.position, FollowDistance);//shows the size of the enemys dection 
+    }
+}
+
