e2cd1ac442bea6400a222d1a9171cf68a65e2fc4
Paulina Wozniak <S00176242@mail.itsligo.ie>
2020-02-02 23:31:13 +0000

    gate

---
+++ Ad Aeternum/Assets/PaulinasWork/Scripts/AttackScript3.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackScript3 : MonoBehaviour
+{
+    GameObject sphere;
+    void Start()
+    {
+        sphere = this.gameObject;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                Destroy(other.gameObject);
+            }
+        }
+    }
+
+    //public void OnCollisionStay(Collision other)
+    //{
+
+    //    if (other.gameObject.tag == "Enemy")
+    //    {
+    //        if (Input.GetKeyDown(KeyCode.Mouse0))
+    //        {
+    //            Destroy(other.gameObject);
+    //        }
+    //    }
+    //}
+}
---
+++ Ad Aeternum/Assets/PaulinasWork/Scripts/OpenGateScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenGateScript : MonoBehaviour
+{
+    public List<GameObject> enemies;
+    List<bool> enemiesBool;
+    GameObject barrier;
+
+    public GameObject enemy1;
+    public GameObject enemy2;
+    public GameObject enemy3;
+
+    void Start()
+    {
+        barrier = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //foreach (GameObject enemy in enemies)
+        //{
+        //    if (enemy == null)
+        //    {
+        //        enemiesBool[enemy.GetComponent<List>().Count(enemy)] = true;
+        //    }
+        //}
+
+        //if (!enemiesBool.Contains(false))
+        //{
+        //    Destroy(barrier);
+        //}
+
+        if (enemy1 == null & enemy2 == null && enemy3 == null)
+        {
+            Destroy(barrier);
+        }
+    }
+}
