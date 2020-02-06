9a50199661c35222fda6fb1952225690df56ebc5
S00181282 <35927198+S00181282@users.noreply.github.com>
2020-02-06 11:54:08 +0000

    Enemy Shooter Test

---
+++ Ad Aeternum/Assets/LukesWork/Scripts/EnemyShootScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShootScript : MonoBehaviour
+{
+    EnemyMover enemyMover;
+    GameObject enemy, instProjectile;
+    public GameObject projectile, player;
+    Rigidbody rb;
+
+    void Start()
+    {
+        enemy = this.gameObject;
+    }
+
+    void Update()
+    {
+        InvokeRepeating("Shoot", 2, 5);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        enemy.transform.LookAt(player.transform);
+    }
+
+    void Shoot()
+    {
+        instProjectile = Instantiate(projectile, enemy.transform.position, Quaternion.identity);
+
+        rb = instProjectile.GetComponent<Rigidbody>();
+
+        rb.AddForce(enemy.transform.forward * 5, ForceMode.Impulse);
+
+        Destroy(instProjectile, 2);
+
+        CancelInvoke();
+    }
+}
