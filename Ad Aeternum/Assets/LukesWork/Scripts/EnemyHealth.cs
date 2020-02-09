a75aae172b03f42ae7deb4e6abc29da4755d348c
Luke O'Reilly <35927198+S00181282@users.noreply.github.com>
2020-02-09 23:22:48 +0000

    Mini Update 14

---
+++ Ad Aeternum/Assets/LukesWork/Scripts/EnemyHealth.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public Vector3 position, diff;
+    public Transform target;
+
+    public GameObject healthBar;
+    List<GameObject> enemies;
+    public Canvas canvas;
+
+    void Update()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        foreach (GameObject enemy in enemies)
+        {
+            GameObject instantiate;
+
+            if (enemy.GetComponent<Renderer>().isVisible)
+            {
+                instantiate = Instantiate(healthBar, canvas.transform);
+                instantiate.transform.localPosition = new Vector3(0, 1, 0);
+            }
+        }
+    }
+
+    //public GameObject GetNearbyEnemies()
+    //{
+    //    GameObject[] gos = GameObject.FindGameObjectsWithTag("Enemy");
+    //    float distance = Mathf.Infinity;
+    //    position = target.position;
+
+    //    foreach (GameObject go in gos)
+    //    {
+    //        diff = go.transform.position - position;
+    //        float curDistance = diff.sqrMagnitude;
+
+            
+    //    }
+
+    //    return closest;
+    //}
+}
