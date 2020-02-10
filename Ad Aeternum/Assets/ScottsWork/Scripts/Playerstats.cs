8c0e03115825df52cdff3fec1a3bcf33239d1e17
Scott Gallagher <35927142+s00178222@users.noreply.github.com>
2020-02-10 11:57:59 +0000

    FIXED

---
+++ Ad Aeternum/Assets/ScottsWork/Scripts/Playerstats.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerstats : MonoBehaviour
+{
+    public int MaxHealth = 100;
+    public int currentHealth { get; private set; }
+
+    private void Awake()
+    {
+        currentHealth = MaxHealth;
+    }
+
+    public void takenDamage ( int damage )
+    {
+        currentHealth -= damage;
+
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public virtual void Die()
+    {
+
+    }
+}
