10f2321ec1f775325e36264bbc930ca2ab5d9abe
S00181282 <35927198+S00181282@users.noreply.github.com>
2020-02-12 15:48:04 +0000

    Psuedo Frustum for Objects but not Terrain

---
+++ Ad Aeternum/Assets/LukesWork/Scripts/ObjectFrustumHider.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFrustumHider : MonoBehaviour
+{
+    bool active;
+    Vector3 scale;
+
+    void Start()
+    {
+        scale = new Vector3(100000, 100000, 100000);
+    }
+
+    private void Update()
+    {
+        transform.localScale = scale;
+
+        if (transform.localScale == scale)
+        {
+            scale = new Vector3(1, 1, 1);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.layer != 10)
+        {
+            if (other.GetComponent<MeshRenderer>() != null)
+                other.GetComponent<MeshRenderer>().enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer != 10)
+        {
+            if (other.GetComponent<MeshRenderer>() != null)
+                other.GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+}
