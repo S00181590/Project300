7fce1ba8ad9fe2f174de370382f8cb6e92fb630e
Scott Gallagher <35927142+s00178222@users.noreply.github.com>
2020-02-15 20:01:34 +0000

    water drop effecht

---
+++ Ad Aeternum/Assets/ScottsWork/Scripts/Music and sound effechts/Audioontrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audioontrigger : MonoBehaviour
+{
+    public float num;
+    public GameObject trigger;
+    public AudioSource audioclip;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Ground")
+        {
+            trigger.SetActive(true);
+            audioclip.Play();
+        }
+    }
+}
