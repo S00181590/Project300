1bf97974e7cd07494db51c77194b43855a64eb3d
Scott Gallagher <35927142+s00178222@users.noreply.github.com>
2020-02-19 15:27:54 +0000

    added new assets

--- Ad Aeternum/Assets/ScottsWork/Scripts/Ai/spider.cs
+++ Ad Aeternum/Assets/ScottsWork/Scripts/Ai/spider.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class spider : NavMeshMover
+public class spider : Player
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        base.Start();
-    }
+    bool isslowed = false;
 
-    // Update is called once per frame
-    void Update()
+
+    public  void OnTriggerEnter(Collider collision)
     {
-        
+        if(collision.name == "Player")
+        {
+            isslowed = true;
+            
+
+        }
     }
 }
