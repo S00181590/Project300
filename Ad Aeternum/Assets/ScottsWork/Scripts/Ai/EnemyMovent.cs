e1a6b7249620927047954d6c530c90eae7e3414d
Scott Gallagher <35927142+s00178222@users.noreply.github.com>
2020-02-26 20:00:28 +0000

    ;lkjhc

---
+++ Ad Aeternum/Assets/ScottsWork/Scripts/Ai/EnemyMovent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovent : NavMeshMover
+{
+
+    public GameObject Player;
+    public float FollowAmount;
+    public Vector3 startPoistion;
+    // Start is called before the first frame update
+  public override  void Start()
+    {
+        startPoistion = transform.position;
+        Player = GameObject.Find("Player");
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position,Player.transform.position) <= FollowAmount)
+        {
+            agent.stoppingDistance = 3.0f;
+            MoveTo(Player);
+        }
+        else
+        {
+            agent.stoppingDistance = 5;
+            MoveTo(startPoistion);
+        }
+    }
+}
