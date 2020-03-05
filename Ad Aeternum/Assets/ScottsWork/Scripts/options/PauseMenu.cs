d5365b15bd2cc130daf96d870c4f7f72f64abdbc
S00181282 <35927198+S00181282@users.noreply.github.com>
2020-03-05 02:48:33 +0000

    Merge

--- Ad Aeternum/Assets/ScottsWork/Scripts/options/PauseMenu.cs
+++ Ad Aeternum/Assets/ScottsWork/Scripts/options/PauseMenu.cs
@@ -2,71 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
 public class PauseMenu : MonoBehaviour
 {
-    
     public string sceneName;
     public GameObject pauseMenu;
     public bool IsPaused;
     public GameObject Options;
     public bool OptionsOpen;
+
     void Start()
     {
-        
         IsPaused = false;
         OptionsOpen = false;
-          
     }
 
-    // Update is called once per frame
-  public  void Update()
+    public void Update()
     {
-        
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             IsPaused = !IsPaused;
-           
+            OptionsOpen = false;
         }
 
         if (IsPaused)
         {
             //Instantiate(pauseMenu, transform);
+            Time.timeScale = 0f;
             pauseMenu.SetActive(true);
-            Time.timeScale = 0f;
         }
-        else if (!IsPaused)
+        else
         {
+            Time.timeScale = 1f;
             pauseMenu.SetActive(false);
-            Time.timeScale = 1f;
-        }
-        else if (OptionsOpen)
-        {
-            Options.SetActive(true);
-            Time.timeScale = 0f;
-        }
-        else if (!OptionsOpen)
-        {
             Options.SetActive(false);
+        }
+
+        if (OptionsOpen)
+        {
+            Time.timeScale = 0f;
+            Options.SetActive(true);
+        }
+        else
+        {
             Time.timeScale = 1f;
+            Options.SetActive(false);
         }
     }
 
     public void ResumeTheGame()
     {
         IsPaused = !IsPaused;
-        if (!IsPaused)
-        {
-            pauseMenu.SetActive(false);
-            Time.timeScale = 1f;
-
-        }
     }
+
     public void Optionsmenu()
     {
 
-        OptionsOpen = true;
-        Options.SetActive(true);
-        Time.timeScale = 0f;
+        OptionsOpen = !OptionsOpen;
 
 
         //if (OptionsOpen)
@@ -80,20 +72,21 @@ public class PauseMenu : MonoBehaviour
         //    Time.timeScale = 1f;
         //}
     }
+
     public void RetuenToMain()
     {
-        SceneManager.LoadScene(sceneName);
         Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneName);
     }
 
     public void SaveSettings()
     {
         OptionsOpen = !OptionsOpen;
-        if (!OptionsOpen)
-        {
-            Options.SetActive(false);
-            Time.timeScale = 1f;
 
-        }
+        //if (!OptionsOpen)
+        //{
+        //    Options.SetActive(false);
+        //    Time.timeScale = 1f;
+        //}
     }
 }
