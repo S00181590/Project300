145a81253c85bde8120bbbf22f4c9269d31093d2
Luke O'Reilly <35927198+S00181282@users.noreply.github.com>
2020-03-14 01:32:44 +0000

    Huge Update 2

---
+++ Ad Aeternum/Assets/LukesWork/Scripts/CloudScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudScript : MonoBehaviour
+{
+    GameObject player;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+    
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.x + 200, player.transform.position.z);
+    }
+}
--- Ad Aeternum/Assets/LukesWork/Scripts/ScrollingTexture.cs
+++ Ad Aeternum/Assets/LukesWork/Scripts/ScrollingTexture.cs
@@ -6,14 +6,21 @@ public class ScrollingTexture : MonoBehaviour
 {
     //Scrolls the entire texture on an object, such as a water effect
 
-    public float ScrollX = 0.5f;
-    public float ScrollY = 0.5f;
+    public float ScrollX = 0.5f, ScrollY = 0.5f;
+    float OffsetX, OffsetY;
+    Renderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
 
     void Update()
     {
-        float OffsetX = Time.time * ScrollX;
-        float OffsetY = Time.time * ScrollY;
+        OffsetX = Time.time * ScrollX;
+        OffsetY = Time.time * ScrollY;
 
-        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(OffsetX, OffsetY);
+        //rend.material.mainTextureOffset = new Vector2(OffsetX, OffsetY);
+        GetComponent<Material>().mainTextureOffset = new Vector2(OffsetX, OffsetY); 
     }
 }
