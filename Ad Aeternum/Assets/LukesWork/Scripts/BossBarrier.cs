eda59f62ea21650142a401bee7f783e55189b2b5
Luke O'Reilly <35927198+S00181282@users.noreply.github.com>
2020-03-16 15:13:12 +0000

    Mini Update 27

---
+++ Ad Aeternum/Assets/LukesWork/Scripts/BossBarrier.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBarrier : MonoBehaviour
+{
+    public string bossName;
+    GameObject boss;
+
+    void Start()
+    {
+        boss = GameObject.Find(bossName);
+    }
+    
+    void Update()
+    {
+        if (boss == null)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
